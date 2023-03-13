e1eaa4eafa87845fa8ce77386bf1db1d7e88bdc1
Arthaz1245 <sergiomendozap96@gmail.com>
2023-03-13 10:32:20 -0500

    hola

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const clothes = require("./clothes");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 4500;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
